1958c555a36b8f59fd8f0338e3075e7f71ab03ac
K Sapede <k.e.sapede@gmail.com>
2013-04-13 22:31:51 -0500

    add all follow/unfollow functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ SampleApp::Application.routes.draw do
   end
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
-  resources :relationships, only: [:create, :destory]
+  resources :relationships, only: [:create, :destroy]
 
   root to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
